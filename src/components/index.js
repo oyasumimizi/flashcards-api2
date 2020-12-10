183ddd5e216bbeddf59821316879271e9bfde463
brooke <daisyrakionvideos@gmail.com>
2020-12-09 21:18:29 -0600

    fixing the repo. previously had an error [if its fixed]

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const jsxElement = <h1>Flash cards App</h1>;
+console.log(jsxElement);
+
+ReactDOM.render(jsxElement, document.getElementById("root"));
+
