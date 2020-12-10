daf396554bc0344e1aaf63a78da42640fc1dbc0d
brooke <daisyrakionvideos@gmail.com>
2020-12-09 19:45:21 -0600

    fixed where previousbutton....jsx was and added more onto app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardCollection from './cardCollection/cardCollection';
 import FlashCards from './flashCards/flashCards';
+import CardPosition from './previousButtonForwardButton';
 
 class App extends Component{
     constructor(props){
@@ -26,3 +27,38 @@ class App extends Component{
             flashCardsNumber: this.flashCards.length - 1
         });
     }
+
+    
+    goToNextFlashCard(){
+        let tempFlashCardsNumber = this.state.flashCardsNumber;
+        tempFlashCardsNumber++;
+        if(tempFlashCardsNumber === this.flashCards.length{
+            tempFlashCardsNumber = 0;
+        }
+        this.setState({
+            flashCardsNumber: tempFlashCardsNumber
+        });
+    }
+
+    goToPreviousFlashCard(){
+        let tempFlashCardsNumber = this.state.flashCardsNumber;
+        tempFlashCardsNumber--;
+        if(tempFlashCardsNumber < 0)
+            tempFlashCardsNumber = this.flashCards.length -1;
+        this.setState({
+            flashCardsNumber: tempFlashCardsNumber
+        });
+    }
+
+    render = (){
+        return (
+           <div className="container-fluid">
+                <CardPosition flashCards={this.flashCards[this.state.flashCardsNumber]} nextCard={() => this.goToNextFlashCard()} previousCard={() => this.goToPreviousFlashCard()}/>)
+                <CardCollection addNewCardCollection={this.addNewCardCollection.bind(this)}/>
+            </div>
+
+        );
+    }
+}
+
+export default App;
